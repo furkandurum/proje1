622f3dc9f46347a8091860b228458146194bc1c5
furkandurum <zfdurum@gmail.com>
2022-10-19 21:13:59 +0300

    first commit

---
+++ video-3-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // preprocess
+
+// Uzaktan Akademi C ile Programlamaya Giriş 
+// Göktuğ Başaran
+
+int main(){ // scope
+
+	printf("C'ye hoşgeldiniz!\n");
+	
+	return 0;
+} // main'in sonu
